b945a9e0506491d28dadbf5b64b14934a965e6b0
Oluoma Ezekwe <o.ezekwe1@universityofgalway.ie>
2024-03-22 13:50:44 +0000

    added styling to welcome screen

--- HabitTracker/app/screens/WelcomeScreen.js
+++ HabitTracker/app/screens/WelcomeScreen.js
@@ -1,47 +1,84 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  Image,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { colours } from "../components/theme";
 
 function WelcomeScreen() {
   const nav = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 26, fontWeight: "bold", marginBottom: 10 }}>
-        Welcome to Habit Tracker
-      </Text>
-      <Text style={{ fontSize: 20, marginBottom: 40 }}>
-        Start Tracking Your Habits Today!
-      </Text>
-      <View style={{ gap: 10 }}>
-        <TouchableOpacity
-          style={styles.touchable}
-          onPress={() => nav.navigate("Login")}
-        >
-          <Text style={{ fontSize: 18 }}>Log In</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.touchable}
-          onPress={() => nav.navigate("Register")}
-        >
-          <Text style={{ fontSize: 18 }}>Register</Text>
-        </TouchableOpacity>
+    <ImageBackground
+      style={styles.background}
+      source={require("../../assets/welcomeBackground.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/welcomeImage.png")}
+        />
+        <Text style={{ fontSize: 42, color: "white" }}>ZenTrack</Text>
+        <Text style={{ fontSize: 26, color: "white" }}>Track your habits!</Text>
       </View>
-      <StatusBar />
-    </View>
+      <TouchableOpacity
+        style={styles.loginButton}
+        onPress={() => nav.navigate("Login")}
+      >
+        <Text style={{ fontSize: 24, color: "white" }}>Log In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.registerButton}
+        onPress={() => nav.navigate("Register")}
+      >
+        <Text style={{ fontSize: 24, color: "white" }}>Register</Text>
+      </TouchableOpacity>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 20,
   },
+  loginButton: {
+    width: "100%",
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#7398b1",
+  },
+  logo: {
+    width: 260,
+    height: 260,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 100,
+    alignItems: "center",
+  },
+  registerButton: {
+    width: "100%",
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#8da85c",
+  },
   touchable: {
-    backgroundColor: "#e0e0e0",
+    backgroundColor: colours.button,
     justifyContent: "center",
     alignItems: "center",
     padding: 12,
