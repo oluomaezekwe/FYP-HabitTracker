e1f10280a9b63b5de51c96b1d62b0a2f3c55b163
Oluoma Ezekwe <o.ezekwe1@universityofgalway.ie>
2024-03-23 20:52:17 +0000

    added styling for home screen, tip display

--- HabitTracker/app/components/HomeScreen/Tips.js
+++ HabitTracker/app/components/HomeScreen/Tips.js
@@ -1,73 +1,99 @@
-import React, { useEffect, useState } from "react";
-import { Dimensions, Pressable, Text, View } from "react-native";
+import React, { useEffect } from "react";
 import {
-  BottomModal,
-  ModalTitle,
-  SlideAnimation,
-  ModalContent,
-} from "react-native-modals";
-import { fetchTips } from "../../context/actions/firebaseActions";
+  ActivityIndicator,
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { useDispatch, useSelector } from "react-redux";
+import { fetchTips } from "../../context/actions/firebaseActions";
 
 function Tips() {
-  const [isModalVisible, setModalVisible] = useState(false);
-  const tips = useSelector((state) => state.tip.tips);
   const dispatch = useDispatch();
+  const tips = useSelector((state) => state.tip.tips);
 
   useEffect(() => {
     // Dispatch the fetchTips action when the component mounts
-    dispatch(fetchTips());
+    try {
+      dispatch(fetchTips());
+    } catch (error) {
+      console.log("Error fetching tips: ", error);
+    }
 
     console.log("Tips:", tips);
   }, [dispatch]);
 
+  const getRandomTip = (array) => {
+    if (!array || array.length === 0) return null; // Return null if array is empty
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  };
+
+  const randomTip = getRandomTip(tips);
+
+  if (!randomTip) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator
+          style={styles.activityIndicator}
+          size="large"
+          color="darkgrey"
+        />
+      </View>
+    );
+  }
+
   return (
-    <>
-      <View>
-        <Pressable
-          onPress={() => setModalVisible(true)}
+    <View style={styles.container}>
+      <View style={{ padding: 18 }}>
+        <View
           style={{
-            width: Dimensions.get("screen").width * 0.85,
-            height: 150,
-            backgroundColor: "lightgray",
-            borderRadius: 15,
-            alignItems: "center",
-            justifyContent: "center",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            gap: 10,
           }}
         >
-          <Text style={{ fontSize: 20, padding: 50 }}>Daily Tip</Text>
-        </Pressable>
-      </View>
-
-      <BottomModal
-        onBackdropPress={() => setModalVisible(!isModalVisible)}
-        onHardwareBackPress={() => setModalVisible(!isModalVisible)}
-        swipeDirection={["up", "down"]}
-        swipeThreshold={200}
-        modalTitle={<ModalTitle title="Daily Tip" />}
-        modalAnimation={
-          new SlideAnimation({
-            slideFrom: "bottom",
-          })
-        }
-        visible={isModalVisible}
-        onTouchOutside={() => setModalVisible(!isModalVisible)}
-      >
-        <ModalContent style={{ width: "100%", height: 600 }}>
-          <View style={{ marginVertical: 10 }}>
-            <Text style={{ fontSize: 18 }}>Read Today's Tip Here!</Text>
-            {tips.map((tip) => (
-              <View key={tip.content} style={{}}>
-                <Text>
-                  {tip.content} {tip.author}
-                </Text>
-              </View>
-            ))}
+          <View style={{ width: 250, paddingTop: 5, paddingLeft: 15 }}>
+            <Text style={{ fontSize: 22 }}>{randomTip.content}</Text>
+            <Text
+              style={{
+                marginTop: 15,
+                fontSize: 18,
+                fontStyle: "italic",
+              }}
+            >
+              {randomTip.author}
+            </Text>
           </View>
-        </ModalContent>
-      </BottomModal>
-    </>
+          <View style={{ width: 120 }}>
+            <Image
+              style={styles.image}
+              source={require("../../../assets/tipImage.png")}
+            />
+          </View>
+        </View>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 375,
+    alignItems: "flex-end",
+    justifyContent: "flex-start",
+  },
+  image: {
+    width: 120,
+    height: 120,
+  },
+  activityIndicator: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 12,
+  },
+});
 
 export default Tips;
--- HabitTracker/app/screens/HomeScreen.js
+++ HabitTracker/app/screens/HomeScreen.js
@@ -1,40 +1,94 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 import Header from "../components/HomeScreen/Header";
 import Habits from "../components/HomeScreen/Habits";
 import Achievements from "../components/HomeScreen/Achievements";
 import Tips from "../components/HomeScreen/Tips";
+import { ScrollView } from "react-native-gesture-handler";
 
 function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Header />
-      <View style={styles.screenCard}>
-        <Habits />
+      <View style={{ paddingHorizontal: 20 }}>
+        <Header />
       </View>
-      <View style={styles.screenCard}>
-        <Achievements />
-      </View>
-      <View style={styles.screenCard}>
-        <Tips />
+      <ScrollView
+        horizontal={true}
+        pagingEnabled={true}
+        snapToAlignment="start"
+        bounces={false}
+        style={{ marginBottom: 20 }}
+      >
+        <View style={[styles.card, styles.elevation, styles.shadowProp]}></View>
+        <View style={[styles.card, styles.elevation, styles.shadowProp]}></View>
+      </ScrollView>
+      <View style={[styles.tipContainer, styles.elevation, styles.shadowProp]}>
+        <View style={styles.tipCard}>
+          <Tips />
+        </View>
       </View>
     </View>
   );
 }
 
+const screenHeight = Platform.OS == "ios" ? 60 : 30;
+
 const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    width: 370,
+    padding: 100,
+    backgroundColor: "white",
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 5,
+    marginBottom: 15,
+    borderRadius: 30,
+  },
   container: {
     flex: 1,
-    paddingTop: 60,
-    paddingHorizontal: 20,
+    backgroundColor: "white",
+    paddingTop: screenHeight,
   },
-  screenCard: {
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 50,
-    marginBottom: 10,
+  // android shadow
+  elevation: {
+    elevation: 20,
+    shadowColor: "#171717",
+  },
+  // ios shadow
+  shadowProp: {
+    shadowColor: "#777779",
+    shadowOffset: { height: 1 },
+    shadowOpacity: 0.4,
+    shadowRadius: 7,
+  },
+  tipContainer: {
+    flex: 1,
+    backgroundColor: "white",
+    marginHorizontal: 10,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    shadowOffset: 12,
+  },
+  tipCard: {
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    paddingHorizontal: 30,
   },
 });
 
 export default HomeScreen;
+
+// <View style={styles.container}>onnnnn
+//   <Header />
+//   <View style={styles.screenCard}>
+//     <Habits />
+//   </View>
+//   <View style={styles.screenCard}>
+//     <Achievements />
+//   </View>
+//   <View style={styles.screenCard}>
+//     <Tips />
+//   </View>
+// </View>
